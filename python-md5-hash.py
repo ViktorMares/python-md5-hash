6367c19198dac9e004439775ccf3b9461c208350
VM <80492489+ViktorMares@users.noreply.github.com>
2023-01-12 11:39:29 +0200

    Update python-md5-hash.py

--- python-md5-hash.py
+++ python-md5-hash.py
@@ -9,10 +9,10 @@ empty_list = []
 # Open the desired file (rockyou.txt in our case) and use the latin 1 encoding to avoid byte errors
 with open('rockyou.txt', 'r', encoding='latin-1') as rockyou:
 	for pw in rockyou:
-		if pw != '':
-			empty_list.append(str(pw.strip()))
+		if pw != '': #We want to skip all empty lines inside rockyou.txt
+			empty_list.append(str(pw.strip())) #Strip any whitespace from the passwords
 
 
 # Go through each password and hash it using the MD5 algorithm
 for password in empty_list:
-	print(hashlib.md5(f'{password}'.encode()).hexdigest())
+	print(hashlib.md5(f'{password}'.encode()).hexdigest()) # We encode (convert to a bytes object) each password (md5 will not accept it otherwise) & print out the result
